e26f1e4cc30f60d2bb6c2bba2e4192fb699e8390
52YvesMahama <39618772+52YvesMahama@users.noreply.github.com>
2018-11-26 22:41:52 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
-
-
-
-let name = 'Yves';
-let firstName = 'Mahama';
-let firstName1 = 'Iragi';
-let firstName2 ='Maradona';
-let age = 40;
-let number = 21;
-let number1 =34;
-let number2 =54;
-let number3 =45;
-
+        // Comment Ignored by JavaScripts
+        console.log('Hello World 52 Yves');
+let name = 'Mahama';
 console.log(name);
-console.log(age);
-console.log(firstName);
-console.log(firstName1);
-console.log(firstName2);
-console.log(number);
-console.log(number1);
-console.log(number2);
-console.log(number3);
 
-console.log(number1 + number);
-console.log(number1 / number);
-console.log(number1 * number);
-console.log(number1 - number);
-console.log(number3 + number);
+// cannot be a reserved keyword.
+
+let firstName = 'Mohombi';
+let lastName = 'Maradona';
+
+console.log (firstName + lastName);
 
 
-let person = {
-    name: 'Kawangware',
-    name1: 'Nairobi',
-    name2: 'Githurai',
-    name3: 'Kawangware',
-    name4: 'Nairobi',
-    name5: 'Githurai',
-    name6: 'Kawangware',
-    name7: 'Nairobi',
-    name8: 'Githurai',
-};
-console.log(person);
+let age = 30;
+let isApproved = false;
+let selectedColor = null;
 
-
-
-// let 
-
-let firstName33 = "mahammaa";
-let lastname22 = "Epolete";
-console.log(firstName33 + lastname22);
---
+++ index1.js
@@ -0,0 +1,3 @@
+
+let name = 'Mahama';
+console.log(name);
